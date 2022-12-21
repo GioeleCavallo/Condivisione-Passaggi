8e5a6bb2300546e960fd85e83a5fd3e630d3c98f
GioeleCavallo <gioelecavallo12@gmail.com>
2022-12-21 14:08:29 +0100

    Created Error Object

---
+++ src/condivisione-passaggi/app/Exceptions/ErrorGeneral.php
@@ -0,0 +1,16 @@
+<?php 
+
+class ErrorGeneral {
+
+    public $title;
+    public $description;
+    public $level;
+
+    function __contruct($title, $description, $level){
+        self::$title = $title;
+        self::$description = $description;
+        self::$level = $level;
+    }
+
+}
+?>
