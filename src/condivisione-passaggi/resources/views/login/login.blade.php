af4bfa0c2f64caa07dec50f41b626b511daee089
GioeleCavallo <gioelecavallo12@gmail.com>
2022-12-31 09:15:50 +0100

    "Remember me" checkbox

--- src/condivisione-passaggi/resources/views/login/login.blade.php
+++ src/condivisione-passaggi/resources/views/login/login.blade.php
@@ -10,14 +10,17 @@
                 <h1 class="mb-3 fw-normal">Login</h1>
             
                 <div class="form-floating">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="email" name="email" >
+                    <input type="email" class="form-control" id="floatingInput" placeholder="email" name="email" required>
                     
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" required>
 
                 </div>
-            
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name="remember" checked>
+                    <label class="form-check-label" for="flexSwitchCheckChecked">Remember me</label>
+                </div>
                 <button class="w-100 btn btn-primary btn-lg" type="submit">Login</button>
                 
             </form>
