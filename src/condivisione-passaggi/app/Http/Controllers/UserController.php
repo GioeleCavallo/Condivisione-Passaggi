fce91c9ed24e20f19bd53117b93b9a6662cb371a
GioeleCavallo <gioelecavallo12@gmail.com>
2022-12-31 13:09:32 +0100

    Added admin controller

--- src/condivisione-passaggi/app/Http/Controllers/UserController.php
+++ src/condivisione-passaggi/app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -18,6 +21,96 @@ class UserController extends Controller
             return view('home.home', ['user' => $user]);
         }
         return redirect('/');
+    }
+
+
+    public function admin()
+    {
+        if (Auth::check()) {
+            // Recupera l'utente corrente autenticato
+            $user = auth()->user();
+            if($user->user_tipo=="admin"){
+                $tipi = $this->selectType();
+
+                // Mostra la pagina personale dell'utente
+                return view('admin.admin', ['user' => $user, 'tipi' => $tipi]);
+            }
+        }
+        return redirect('/');
+    }
+
+
+    private function selectType(){
+
+        return DB::table('tipo')
+        ->select('nome')
+        ->get();
+    }
+
+    private function validateInput(Request $request){
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'tipo' => 'required',
+        ]);
+        return $validator;
+    }
+
+    public function addUser(Request $request){
+
+        $validator = $this->validateInput($request);
+
+        if ($validator->fails()) {
+            return redirect('admin')
+            ->with("errors",["Insert valid inputs"])
+            ->withInput();
+        }
+        $validated = $validator->validated();
+        if($this->exists($validated['email'])){
+            return redirect('admin')
+            ->with("errors",["Email already exists"])
+            ->withInput();
+
+        }
+
+        $password = $this->generateRandomPassword(22);
+
+        $this->addUserToDB($validated['name'],$validated['email'],Hash::make($password),$validated['tipo']);
+        $this->sendMail($validated['email'],$validated['name'],$password);
+        return redirect("admin")->withSuccess('User added');
+    }
+
+    private function sendMail($email,$name,$psw){
+
+        $subject = 'Iscrizione';
+        $body = "Ciao $name sei stato iscritto alla condivisione dei passaggi. ecco la tua password: $psw. 
+        Cambiala il prima possibile in ".url('/').".";
+        
+        Mail::raw($body, function($message) use ($email, $subject) {
+            $message->to($email)->subject($subject);
+        });
+    }
+
+    private function generateRandomPassword($length) {
+        $password = '';
+        for ($i = 0; $i < $length; $i++) {
+            $password .= chr(rand(33, 126));
+        }
+        return $password;
+    }
+
+    private function exists($email){
+        return DB::table('users')->where('email', $email)->exists();
+    }
+
+    private function addUserToDB($name,$email,$psw,$type){
+        DB::table("users")->insert([
+            'name' => $name,
+            'email' => $email,
+            'password' => $psw,
+            'user_tipo' => $type
+        ]);
     }
 
     public function logout(Request $request)
