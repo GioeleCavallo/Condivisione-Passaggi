6d30cb466c8c6605ef3b35d2e33318281c540ada
GioeleCavallo <gioelecavallo12@gmail.com>
2022-12-31 09:14:45 +0100

    session handling

--- src/condivisione-passaggi/app/Http/Controllers/LoginController.php
+++ src/condivisione-passaggi/app/Http/Controllers/LoginController.php
@@ -7,12 +7,20 @@ use App\Http\Controllers\Controller;
 use App\Exceptions\ErrorGeneral;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Validator;
 
 class LoginController extends Controller
 {
     public function index(){
-        return view('login.login');
+        // Verifica se l'utente è autenticato
+        if (Auth::check()) {
+            // L'utente è autenticato, lo reindirizzo verso la pagina desiderata
+            return redirect('/homePage');
+        } else {
+            // L'utente non è autenticato, mostra la homepage
+            return view('login.login');
+        }
     }
 
     private function rightLogin($email, $psw){
@@ -21,38 +29,25 @@ class LoginController extends Controller
                 ->first();
         if($user == null){return false;}
         return Hash::check($psw, $user->password); 
-       /* foreach ($users as $user) {
-            if (Hash::check($psw, $user->password)) {
-                return true;
-            }
-        }*/
-       // return false;
     }
 
     public function checkLogin(Request $request)
     {
-        //$input =$request->all() ;
+        
         $messages = array();
-        
-      
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required'
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
         ]);
-        
-        if ($validator->fails()) {
-            $messages[] = "Set correct fields inputs";
-        } else{
-            if($this::rightLogin($request->email, $request->password)){
-                echo "logged in";//return view('admin');
-                return;
-            }else{
-                $messages[] = "Wrong username or password";
-            }
+ 
+        if (Auth::attempt($credentials, $request->has('admin'))) {
+            return redirect()->intended('homePage');
         }
-        //$messages = "email";
+
+    
+        $messages[] = "Check credentials";
         return back()->with('errors', $messages);
         
-    
     }
 }
