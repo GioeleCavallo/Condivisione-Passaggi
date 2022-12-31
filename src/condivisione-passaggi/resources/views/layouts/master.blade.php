ed14c224010254d78a991887fac28b84659f3330
GioeleCavallo <gioelecavallo12@gmail.com>
2022-12-31 13:10:05 +0100

    added jquery

--- src/condivisione-passaggi/resources/views/layouts/master.blade.php
+++ src/condivisione-passaggi/resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('title')</title>
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
